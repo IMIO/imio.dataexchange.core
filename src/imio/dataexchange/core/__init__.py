8b34b89ca886f05ee691e8feb3e8b0f58a7aa551
Martin Peeters <martin.peeters@affinitic.be>
2014-10-14 14:28:52 +0200

    Fix API

--- src/imio/dataexchange/core/__init__.py
+++ src/imio/dataexchange/core/__init__.py
@@ -1,11 +1,16 @@
 # encoding: utf-8
 
-
+from imio.dataexchange.core.dms import DMSFile
+from imio.dataexchange.core.dms import Invoice
+from imio.dataexchange.core.dms import IncomingMail
+from imio.dataexchange.core.dms import OutgoingMail
 from imio.dataexchange.core.document import Document
-from imio.dataexchange.core.invoice import Invoice
 
 
 __all__ = (
+    DMSFile.__name__,
     Document.__name__,
     Invoice.__name__,
+    IncomingMail.__name__,
+    OutgoingMail.__name__,
 )
