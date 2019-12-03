ca9cb9acae3cbbfa87d894aa132411fbadb09e71
Martin Peeters <martin.peeters@affinitic.be>
2019-12-03 13:07:29 +0100

    Make the dispatcher resilient to RabbitMQ restart

--- src/imio/dataexchange/core/scripts/base.py
+++ src/imio/dataexchange/core/scripts/base.py
@@ -4,6 +4,7 @@ from ConfigParser import ConfigParser
 from imio.dataexchange.db import DBSession
 from imio.dataexchange.db import DeclarativeBase
 from imio.dataexchange.db.mappers.file import File
+from pika.exceptions import AMQPConnectionError
 from sqlalchemy import distinct
 from sqlalchemy import engine_from_config
 
@@ -38,13 +39,16 @@ def get_client_ids(file_type):
     return [l[0] for l in query.all()]
 
 
-def init_dispatcher(
-    config, dispatcher_cls, consumer_cls, publisher_cls, file_type,
-):
-    amqp_url = config.get('app:main', 'rabbitmq.url')
-    dispatcher = dispatcher_cls(consumer_cls, publisher_cls,
-                                '{0}/%2Fwebservice?connection_attempts=3&'
-                                'heartbeat_interval=3600'.format(amqp_url))
+def generate_dispatcher(dispatcher_cls, consumer_cls, publisher_cls, url):
+    params = "connection_attempts=3&heartbeat_interval=3600"
+    dispatcher = dispatcher_cls(
+        consumer_cls, publisher_cls, "{0}/%2Fwebservice?{1}".format(url, params)
+    )
+    return dispatcher
+
+
+def init_dispatcher(config, dispatcher_cls, consumer_cls, publisher_cls, file_type):
+    amqp_url = config.get("app:main", "rabbitmq.url")
     client_ids = get_client_ids(file_type)
     wait = 0
     while not client_ids:
@@ -52,13 +56,26 @@ def init_dispatcher(
         sleep_duration = wait * 60
         if sleep_duration > 360:
             sleep_duration = 360
-        print 'There is no message to publish for type ({0})'.format(file_type)
+        print("There is no message to publish for type ({0})".format(file_type))
         time.sleep(sleep_duration)
         client_ids = get_client_ids(file_type)
-    for client_id in client_ids:
-        queue_name = '{0}.{1}'.format(consumer_cls.queue, client_id)
-        dispatcher.publisher.setup_queue(queue_name, client_id)
-    try:
-        dispatcher.start()
-    except KeyboardInterrupt:
-        dispatcher.stop()
+    while True:
+        try:
+            dispatcher = generate_dispatcher(
+                dispatcher_cls, consumer_cls, publisher_cls, amqp_url
+            )
+            for client_id in client_ids:
+                queue_name = "{0}.{1}".format(consumer_cls.queue, client_id)
+                dispatcher.publisher.setup_queue(queue_name, client_id)
+            dispatcher.start()
+        except AMQPConnectionError:
+            if getattr(dispatcher, "_connection", False):
+                try:
+                    dispatcher.stop()
+                except AttributeError:
+                    # This error happen when RabbitMQ is not ready yet
+                    pass
+            time.sleep(1)
+        except KeyboardInterrupt:
+            dispatcher.stop()
+            break
